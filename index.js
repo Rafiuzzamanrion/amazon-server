7071e581948215e7a1c9560c2136a9cd8ae11fd6
Your Name <rafiujjaman5683@gmail.com>
2024-02-07 00:08:03 +0600

    payment gateway route added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-
+const SSLCommerzPayment = require('sslcommerz-lts')
 
 
 
@@ -28,6 +28,11 @@ const client = new MongoClient(uri, {
   }
 });
 
+// ----------- ssl commerz ----------------
+const store_id = process.env.STORE_ID
+const store_passwd = process.env.STORE_PASS
+const is_live = false //true for live, false for sandbox
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -38,6 +43,7 @@ async function run() {
     const usersCollection = client.db('amazon-latest-DB').collection('users');
     const reviewsCollection = client.db('amazon-latest-DB').collection('reviews');
     const cartCollection = client.db('amazon-latest-DB').collection('cart');
+    const paymentCollection = client.db('amazon-latest-DB').collection('payment');
 
 
     // --------------- products collection ----------------
@@ -55,6 +61,21 @@ async function run() {
     const result = await productsCollection.findOne(query);
     res.send(result);
   });
+
+  // ------------ post a product to database ---------
+      app.post('/product',async(req,res)=>{
+        const productData = req.body;
+        const result = await productsCollection.insertOne(productData);
+        res.send(result);
+      });
+
+      // ---------- delete a product from database ----------
+      app.delete('/deleteProduct',async(req,res)=>{
+        const id = req.query.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await productsCollection.deleteOne(query);
+        res.send(result);
+      })
     //  ---------------- end products collection----------------
 
 
@@ -159,8 +180,107 @@ async function run() {
     res.send(result);
   });
 
+  app.delete('/deleteCarts',async(req,res)=> {
+    const email = req.query.email;
+    const query = {email : email}
+    const result = await cartCollection.deleteMany(query);
+    res.send(result);
+  });
+  // --------------- payment collection -------------------
+
+  app.post('/postPayment', async(req,res)=> {
+    const paymentData = req.body;
+    const {name,time,amount,email,number,postcode,currency,address} = paymentData;
+    const trans_id = new ObjectId().toString();
+    const data = {
+      total_amount: paymentData?.amount,
+      currency: paymentData.currency,
+      tran_id: trans_id, // use unique tran_id for each api call
+      success_url: `http://localhost:5000/payment/success/${trans_id}`,
+      fail_url: `http://localhost:5000/payment/failed/${trans_id}`,
+      cancel_url: 'http://localhost:5000/cancel',
+      ipn_url: 'http://localhost:5000/ipn',
+      shipping_method: 'Courier',
+      product_name: 'Computer.',
+      product_category: 'Electronic',
+      product_profile: 'general',
+      cus_name: paymentData.name,
+      cus_email: paymentData.email,
+      cus_add1: paymentData.address,
+      cus_add2: 'Dhaka',
+      cus_city: 'Dhaka',
+      cus_state: 'Dhaka',
+      cus_postcode: paymentData.postcode,
+      cus_country: 'Bangladesh',
+      cus_phone: paymentData.number,
+      cus_fax: '01711111111',
+      ship_name: 'Customer Name',
+      ship_add1: 'Dhaka',
+      ship_add2: 'Dhaka',
+      ship_city: 'Dhaka',
+      ship_state: 'Dhaka',
+      ship_postcode: 1000,
+      ship_country: 'Bangladesh',
+  };
+console.log(data)
+  const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+  sslcz.init(data).then(apiResponse => {
+      // Redirect the user to payment gateway
+      let GatewayPageURL = apiResponse.GatewayPageURL
+      res.send({url:GatewayPageURL})
+
+      const finalOrder = {
+        name:name,
+        email:email,
+        time:time,
+        amount:amount,
+        number:number,
+        postcode:postcode,
+        currency:currency,
+        address:address,
+        paidStatus:false,
+        transactionId:trans_id
+      }
+      const result = paymentCollection.insertOne(finalOrder)
+      console.log('Redirecting to: ', GatewayPageURL)
+  });
 
 
+app.post('/payment/success/:tranId',async(req,res) => {
+console.log(req.params.tranId)
+const result = await paymentCollection.updateOne({transactionId: req.params.tranId},{
+  $set:{
+    paidStatus:true
+  }
+})
+
+if(result.modifiedCount > 0){
+res.redirect(`http://localhost:5173/payment/success/${req.params.tranId}`)
+}
+});
+
+
+
+app.post('/payment/failed/:tranId',async(req,res) => {
+console.log(req.params.tranId)
+const result = await paymentCollection.deleteOne({transactionId: req.params.tranId})
+
+if(result.deletedCount){
+res.redirect(`http://localhost:5173/payment/failed/${req.params.tranId}`)
+}
+})
+
+    // const result = await paymentCollection.insertOne(paymentData);
+    // res.send(result);
+  });
+
+  app.get('/loadSpecificCustomerPayment',async(req,res)=> {
+    const email = req.query.email;
+    const data = await paymentCollection.find()
+    const query = {email : email};
+    const result = await paymentCollection.find().toArray();
+    res.send(result);
+  })
 
 
 
